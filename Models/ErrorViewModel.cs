2cadb252f44d28bb0c76f6096033d4e17aeb0acc
Gift <gift.maputle@ctucareer.co.za>
2023-11-22 20:03:21 +0200

    Create System Design for documentation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LifeHospital.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
