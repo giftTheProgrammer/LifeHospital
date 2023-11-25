e817ac51e4014e2e82c24dcee5a2d0cfb14e48c1
Gift <gift.maputle@ctucareer.co.za>
2023-11-25 21:31:35 +0200

    Add access control (redirect unathenticated users to the login page) and retrieve userID of authenticated user after login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,27 @@
-﻿using LifeHospital.Models;
+﻿using LifeHospital.Areas.Identity.Data;
+using LifeHospital.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace LifeHospital.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<LifeHospitalUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<LifeHospitalUser> userManager)
         {
             _logger = logger;
+            this._userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
             return View();
         }
 
