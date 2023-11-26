fb38c56c7d4ad31e81586b63e53eae611b0028ae
giftTheProgrammer <m.g.moletsane@gmail.com>
2023-11-26 13:14:07 +0200

    Add model for diagnosis

---
+++ Models/Diagnosis.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LifeHospital.Models
+{
+    public class Diagnosis
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PatientName { get; set; }
+        public string diagnosis { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfDiagnosis { get; set; }
+
+        public string recommendedTreatment { get; set; }
+        public string Prescription { get; set; }
+        public string EmployeeID { get; set;}
+    }
+}
